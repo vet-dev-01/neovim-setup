fde14fa12d3babdca7853cdffeb5225e9c02a4d9
Vet Dev <vetdev01@gmail.com>
2023-12-26 23:54:17 -0500

    First commit for neovim setup

---
+++ init.lua
@@ -0,0 +1,1 @@
+require("vet_dev")
---
+++ lua/vet_dev/init.lua
@@ -0,0 +1,35 @@
+print("Hello Vet Dev!")
+
+require("vet_dev.set")
+require("vet_dev.remap")
+
+local augroup = vim.api.nvim_create_augroup
+local VetDevGroup = augroup('VetDev', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = VetDevGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
